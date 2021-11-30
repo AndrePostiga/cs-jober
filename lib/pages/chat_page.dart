2136b5ca607f496444ae9ae8f6bf31a287ae7199
Pedro Bazilio <pedrobazilio@id.uff.br>
2021-11-30 16:53:46 -0300

    barra de pesquisa adicionada

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:grupolaranja20212/models/chat_users_model.dart';
 import 'package:grupolaranja20212/utils/app_navigator.dart';
 
 class ChatPage extends StatefulWidget{
@@ -9,6 +10,15 @@ class ChatPage extends StatefulWidget{
 }
 
 class _ChatPageState extends State<ChatPage>{
+  List<ChatUsers> chatUsers = [
+    ChatUsers(name: "Albert", msgTxt: "Olá!, Bem-vindo ao processo seletivo", imageUrl:"images/image1.jpeg", time: "15:01"),
+    ChatUsers(name: "Alan", msgTxt: "Fala amigo, o processo pro estágio ta rolando", imageUrl:"images/image2.jpeg", time: "09:23"),
+    ChatUsers(name: "Marie", msgTxt: "Tudo bem, aguardamos seu retorno", imageUrl:"images/image3.jpeg", time:"Ontem"),
+    ChatUsers(name: "Steve", msgTxt: "Bom dia candidato, o feedback irá para o seu email", imageUrl:"images/image4.jpeg", time:"23/10/21"),
+    ChatUsers(name: "Amanda", msgTxt: "Olá candidato", imageUrl:"images/image5.jpeg", time:"23/10"),
+    ChatUsers(name: "Julia", msgTxt: "Somos da empresa Alfa e gostamos muito do seu perfil", imageUrl:"images/image5.jpeg", time:"20/10/21"),
+    ChatUsers(name: "Steve", msgTxt: "Qualquer dúvida entre em contato conosco", imageUrl:"images/image6.jpeg", time:"13/09/21"),
+  ];
   @override
   Widget build (BuildContext context){
     return  Scaffold(
@@ -41,10 +51,30 @@ class _ChatPageState extends State<ChatPage>{
                     )
                   ],
                 ),
-              ))
+              )),
+              Padding(
+                padding: const EdgeInsets.only(top: 16, left: 16, right: 16),
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: "Pesquisar...",
+                    hintStyle: TextStyle(color: Colors.grey.shade500),
+                    prefixIcon: Icon(Icons.search,color: Colors.grey.shade600, size: 20,),
+                    filled: true,
+                    fillColor: Colors.grey.shade100,
+                    contentPadding: const EdgeInsets.all(8),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: BorderSide(
+                        color: Colors.grey.shade100
+                      )
+                    ),
+                  ),
+                ),
+              ),              
           ],
         ),
       ),
     );
   }
 }
+
