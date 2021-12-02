9275e0c546e723d2aeb327312b27a67fb8428666
Igor Lisboa <igor.rodrigues.lisboa@gmail.com>
2021-12-02 02:00:22 -0300

    Alterando model do match

--- lib/models/match.dart
+++ lib/models/match.dart
@@ -1,21 +1,17 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Match {
-  final String recruiterUserId;
-  final String candidateUserId;
-
+  final List<String> usersId;
+  final DateTime createdAt;
   DocumentReference? reference;
 
-  Match(this.recruiterUserId, this.candidateUserId, [this.reference]);
+  Match(this.usersId, this.createdAt, [this.reference]);
 
   Map<String, dynamic> toMap() {
-    return {
-      "recruiterUserId": recruiterUserId,
-      "candidateUserId": candidateUserId
-    };
+    return {"recruiterUserId": usersId, "createdAt": createdAt};
   }
 
   factory Match.fromSnapshot(QueryDocumentSnapshot doc) {
-    return Match(doc["recruiterUserId"], doc["candidateUserId"], doc.reference);
+    return Match(doc["usersId"], doc["createdAt"], doc.reference);
   }
 }
