6727b8cc6a4385e30658241c03e0918cf1827084
gustavocmiranda <gustavocataldomiranda@gmail.com>
2022-01-03 16:24:44 -0300

    alteracoes no mapa
    agora o mapa funciona.
    algumas linhas estao comentadas, pq nao sabia oq elas faziam antes e estavam dando erro

--- lib/pages/matches_map_page.dart
+++ lib/pages/matches_map_page.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart' as latLng;
 import 'package:grupolaranja20212/utils/app_navigator.dart';
 import 'package:grupolaranja20212/view_models/matches_map_view_model.dart';
 
@@ -12,7 +13,7 @@ class MatchesMapPage extends StatefulWidget {
 }
 
 class _MatchesMapPage extends State<MatchesMapPage> {
-  final MatchesMapViewModel _vM = MatchesMapViewModel();
+  /*final MatchesMapViewModel _vM = MatchesMapViewModel();
   List<Marker> allMarkers = [];
 
   late GoogleMapController _controller;
@@ -42,21 +43,44 @@ class _MatchesMapPage extends State<MatchesMapPage> {
     });
 
     await _initVars();
-  }
+  }*/
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: SizedBox(
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-        child: GoogleMap(
-          initialCameraPosition:
-              const CameraPosition(target: LatLng(0, 0), zoom: 12.0),
-          markers: Set.from(allMarkers),
-          onMapCreated: mapCreated,
-        ),
+    return Scaffold(
+        body: FlutterMap(
+      options: MapOptions(
+        center: latLng.LatLng(-22.9041, -43.1327),
+        zoom: 13.0,
       ),
-    );
+      layers: [
+        TileLayerOptions(
+          urlTemplate:
+              "https://api.mapbox.com/styles/v1/gustavocm/ckxsf043a2or315pe4aq6x3qd/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiZ3VzdGF2b2NtIiwiYSI6ImNreHNleDRiYTVhbm0yd3E5c3RtOXZobmgifQ.j6aqVZQh7XisoDs8yP6JLw",
+          additionalOptions: {
+            'accessToken':
+                'pk.eyJ1IjoiZ3VzdGF2b2NtIiwiYSI6ImNreHNleDRiYTVhbm0yd3E5c3RtOXZobmgifQ.j6aqVZQh7XisoDs8yP6JLw',
+            'id': 'mapbox.mapbox-streets-v8'
+          },
+          attributionBuilder: (_) {
+            return Text("© OpenStreetMap contributors");
+          },
+        ),
+        MarkerLayerOptions(
+          markers: [
+            Marker(
+              width: 50.0,
+              height: 50.0,
+              point: latLng.LatLng(-22.9041, -43.1327),
+              builder: (ctx) => Container(
+                child: Image.asset(
+                  "images/pin.png",
+                ),
+              ),
+            )
+          ],
+        ),
+      ],
+    ));
   }
 }
